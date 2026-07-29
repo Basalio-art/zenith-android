5c92a57a5c4fb0ff11f85aa8ebb85b2e66a5f408
Basalio-art <basalioervin@gmail.com>
2026-07-29 13:48:50 +0800

    1785304130 [skip ci]

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { SplashScreen } from '@capacitor/spash-screen';
+import { SplashScreen } from '@capacitor/splash-screen';
 import { Capacitor } from '@capacitor/core';
 import './index.css';
 import App from './App.jsx';
